039c9b6fd8c94bf30ebc455aa880bc3f6b65fee9
Your Name <you@example.com>
2024-11-18 23:16:48 +0500

    initial commut

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
-        <div className="bg-gray-400 w-full h-16 text-center text-2xl md:bg-gray-400 md:w-full md:h-20 md:text-4xl lg:text-6xl lg:bg-gray-400 lg:w-full lg:h-24 py-4">
-            <h1>RESPONSIVE DESIGN</h1>
+        <div className="bg-gray-400 w-full h-16 text-center text-2xl md:bg-gray-400 md:w-full md:h-20 md:text-4xl lg:text-6xl lg:bg-gray-400 lg:w-full lg:h-24 py-4 ">
+            <h1 >RESPONSIVE DESIGN</h1>
         </div>
     )
 }
